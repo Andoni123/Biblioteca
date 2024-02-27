ac45c6b0bec875080cdcaf79f866d59b47d275a8
plaiaundi <plaiaundi@MININT-5OLIS4G>
2024-02-27 14:14:45 +0100

    primer commit

---
+++ Biblio_ED/src/main/Main.java
@@ -0,0 +1,53 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+	static Scanner scan = new Scanner(System.in);
+
+	public static void main(String[] args) {
+
+		int option;
+
+		final int AÑADIR_LIBRO = 1;
+		final int ELIMINAR_LIBRO = 2;
+		final int SALIR = 0;
+		do {
+
+			System.out.println(SALIR + "Salir");
+			System.out.println(AÑADIR_LIBRO + "Añadir libro");
+			System.out.println(ELIMINAR_LIBRO + "Eliminar libro");
+			option = Integer.parseInt(scan.nextLine());
+
+			switch (option) {
+			case AÑADIR_LIBRO:
+
+				break;
+			case ELIMINAR_LIBRO:
+
+				ArrayList<Libro> libros = new ArrayList<Libro>();
+				System.out.println("Introduce id del libro");
+				int idLibro = Integer.parseInt(scan.nextLine());
+
+				for (Libro libro : libros) {
+
+					if (idLibro == libro.getId()) {
+
+						libros.remove(libro);
+						System.out.println("libro eliminado");
+					}
+				}
+
+				break;
+			case SALIR:
+				System.out.println("Salir....");
+				break;
+			default:
+				break;
+			}
+		} while (option != 0);
+	}
+
+}
